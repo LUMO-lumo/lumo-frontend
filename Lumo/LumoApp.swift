1aa456ce1a55e9ee0a2eee0f8857fd677eebdee1
tmdrua02 <ksg072896@gmail.com>
2026-01-02 18:50:48 +0900

    [feat] initial-setting: 프로젝트 초기 설정 및 파일 업로드

---
+++ Lumo/LumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoApp.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
