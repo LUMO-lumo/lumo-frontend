ed34d5f18d99da8ae4e4a52b8a04c363efff300c
tmdrua02 <ksg072896@gmail.com>
2026-01-08 02:09:20 +0900

    [Feat]: 미션 예시 공통 화면 및 수학 미션 화면 추가

--- Lumo/InitialSetup/Mission/OxMissionView.swift
+++ Lumo/InitialSetup/Mission/OxMissionView.swift
@@ -4,4 +4,14 @@
 //
 //  Created by 김승겸 on 1/5/26.
 //
+import SwiftUI
 
+struct OxMissionView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    OxMissionView()
+}
--- Lumo/InitialSetup/Mission/TypingMissionView.swift
+++ Lumo/InitialSetup/Mission/TypingMissionView.swift
@@ -4,4 +4,14 @@
 //
 //  Created by 김승겸 on 1/5/26.
 //
+import SwiftUI
 
+struct TypingMissionView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    TypingMissionView()
+}
