04868cef0e0c9e27e0038b3444999b315b2e4171
yookdoyeon <yookdy2345@gmail.com>
2026-01-09 23:19:37 +0900

    Initial Commit

---
+++ LUMO_MainDevTests/LUMO_MainDevTests.swift
@@ -0,0 +1,17 @@
+//
+//  LUMO_MainDevTests.swift
+//  LUMO_MainDevTests
+//
+//  Created by 육도연 on 1/9/26.
+//
+
+import Testing
+@testable import LUMO_MainDev
+
+struct LUMO_MainDevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
