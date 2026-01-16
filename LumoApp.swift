0ac7d0f9c52ebc5c2b4dfa6f5f4133eb0cd2043f
tmdrua02 <ksg072896@gmail.com>
2026-01-16 23:47:16 +0900

    [Feat]: 닉네임 설정 추가

---
+++ LumoApp.swift
@@ -0,0 +1,47 @@
+//
+//  LumoApp.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/2/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LumoApp: App {
+    @State private var onboardingViewModel = OnboardingViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack(path: $onboardingViewModel.path) {
+                
+                OnBoardingView()
+                    .navigationDestination(for: OnboardingStep.self) { step in
+                        switch step {
+                            
+                        case .initialSetup:
+                            InitialSetupContainerView()
+                            
+                        case .introMission:
+                            MissionIntroView()
+                            
+                        case .missionSelect:
+                            MissionContainerView()
+                            
+//                        case .missionPreview:
+//                            MissionPreviewView()
+                            
+                        case .finalComplete:
+                            FinalCheckView()
+                            
+                        default:
+                            EmptyView()
+                        }
+                    }
+            }
+            .environment(onboardingViewModel)
+        }
+        .modelContainer(for: UserModel.self)
+    }
+}
