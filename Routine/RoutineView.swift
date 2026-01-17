f798869db4e1d262229805601b92d178d7e5f09f
tmdrua02 <ksg072896@gmail.com>
2026-01-17 20:21:40 +0900

    [Feat]: 루틴 폴더 생성

---
+++ Routine/RoutineView.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineView.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/17/26.
+//
+
+import SwiftUI
+
+struct RoutineView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RoutineView()
+}
