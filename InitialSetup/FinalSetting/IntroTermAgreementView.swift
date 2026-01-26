0d92fccea6af2d5ec7b1f1213977308dc4935d0b
tmdrua02 <ksg072896@gmail.com>
2026-01-26 11:02:34 +0900

    [Feat]: 약관 동의 화면 파일 추가

---
+++ InitialSetup/FinalSetting/IntroTermAgreementView.swift
@@ -0,0 +1,14 @@
+//
+//  IntroTermAgreementView.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/26/26.
+//
+
+import SwiftUI
+
+struct IntroTermAgreementView: View {
+    var body: some View {
+        Text("IntroTermAgreementView")
+    }
+}
