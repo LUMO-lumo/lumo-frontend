06cc402cc9703d4c9afcda951e21804b37b1f450
tmdrua02 <ksg072896@gmail.com>
2026-01-27 12:16:03 +0900

    [Perf]: 커스텀 컬러 추가

--- Resources/Colors/Colors.swift
+++ Resources/Colors/Colors.swift
@@ -21,5 +21,25 @@ extension Color {
         
         self.init(red: r, green: g, blue: b)
     }
+    
+    static let main300 = Color(hex: "F55641")
+    
+    static let main200 = Color(hex: "F59241")
+    
+    static let main100 = Color(hex: "FFE747")
+    
+    static let gray800 = Color(hex: "25272A")
+    
+    static let gray700 = Color(hex: "404347")
+    
+    static let gray600 = Color(hex: "7A7F88")
+    
+    static let gray500 = Color(hex: "979DA7")
+    
+    static let gray400 = Color(hex: "BBC0C7")
+    
+    static let gray300 = Color(hex: "DDE1E8")
+    
+    static let gray200 = Color(hex: "F2F4F7")
 }
 
