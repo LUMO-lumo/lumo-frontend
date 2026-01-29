ef3f303c4110e8946acf19617a0b70521d63bd02
tmdrua02 <ksg072896@gmail.com>
2026-01-30 02:40:35 +0900

    [Perf]: 로그인 섹션 별도 View파일로 분리

--- Setting/View/ProfileSettingView.swift
+++ Setting/View/ProfileSettingView.swift
@@ -19,29 +19,7 @@ struct ProfileSettingView: View {
                 ScrollView {
                     VStack(spacing: 24) {
                         
-                        // 로그인 섹션
-                        HStack(spacing: 13) {
-                            // 프로필 이미지 (기본 이미지, 클릭 X)
-                            Circle()
-                                .frame(width: 42, height: 42)
-                                .foregroundColor(.gray300)
-                            
-                            
-                            NavigationLink(destination: Text("로그인 화면")) { // 나중에 LoginView()로 교체
-                                HStack {
-                                    Text("로그인이 필요해요")
-                                        .font(.Subtitle2)
-                                        .foregroundColor(.black)
-                                    
-                                    Spacer()
-                                    
-                                    Image("chevronRight")
-                                        .foregroundColor(.gray500)
-                                }
-                            }
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.top, 17)
+                        LoginSectionView()
                         
                         // 여기에 추후 [PRO 업그레이드], [고급 설정] 등이 추가됩니다.
                         
@@ -49,6 +27,7 @@ struct ProfileSettingView: View {
                 }
             }
         }
+        .padding(.horizontal, 24)
     }
 }
 
