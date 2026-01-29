c2ea77482d5d732f25bcbd0a1260cbae6c29970d
tmdrua02 <ksg072896@gmail.com>
2026-01-30 02:39:46 +0900

    [Feat]: 로그인 섹션 구현

---
+++ Setting/View/Login/LoginSectionView.swift
@@ -0,0 +1,34 @@
+//
+//  LoginSectionView.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/30/26.
+//
+import SwiftUI
+
+struct LoginSectionView : View {
+    var body: some View {
+        // 로그인 섹션
+        HStack(spacing: 13) {
+            // 프로필 이미지 (기본 이미지, 클릭 X)
+            Circle()
+                .frame(width: 42, height: 42)
+                .foregroundColor(.gray300)
+            
+            
+            NavigationLink(destination: Text("로그인 화면")) { // 나중에 LoginView()로 교체
+                HStack {
+                    Text("로그인이 필요해요")
+                        .font(.Subtitle2)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Image("chevronRight")
+                        .foregroundColor(.gray500)
+                }
+            }
+        }
+        .padding(.top, 9)
+    }
+}
