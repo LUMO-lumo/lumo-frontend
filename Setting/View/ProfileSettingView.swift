ab90d9af60a77fad5f1469b8722f1a945407f176
tmdrua02 <ksg072896@gmail.com>
2026-01-30 15:00:10 +0900

    [Feat]: 고급 설정 섹션 구현 #5

--- Setting/View/ProfileSettingView.swift
+++ Setting/View/ProfileSettingView.swift
@@ -25,6 +25,8 @@ struct ProfileSettingView: View {
                         
                         AdvancedSectionView()
                         
+                        SupportSectionView()
+                        
                     }
                 }
             }
---
+++ Setting/View/Support/SupportSectionView.swift
@@ -0,0 +1,41 @@
+//
+//  SupportSectionView.swift
+//  Lumo
+//
+//  Created by 김승겸 on 1/30/26.
+//
+
+import SwiftUI
+
+struct SupportSectionView: View {
+    var body: some View {
+        HStack {
+            
+            NavigationLink(destination: Text("자주 묻는 질문")) {
+                Text("자주 묻는 질문")
+                    .font(.Body1)
+                    .padding(.vertical, 18)
+                    .foregroundStyle(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray400)
+                    .cornerRadius(8)
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: Text("BM")) {
+                Text("BM")
+                    .font(.Body1)
+                    .padding(.vertical, 18)
+                    .foregroundStyle(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray400)
+                    .cornerRadius(8)
+            }
+        }
+    }
+}
+
+#Preview {
+    SupportSectionView()
+}
