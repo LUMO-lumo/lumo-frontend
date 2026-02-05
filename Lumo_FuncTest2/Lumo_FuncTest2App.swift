ddccbb193becc13193b53d5e3b1affe504d15f62
yookdoyeon <yookdy2345@gmail.com>
2026-02-06 00:09:34 +0900

    Initial Commit

---
+++ Lumo_FuncTest2/Lumo_FuncTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lumo_FuncTest2App.swift
+//  Lumo_FuncTest2
+//
+//  Created by 육도연 on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Lumo_FuncTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
